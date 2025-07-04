815b1e5344b82e8e68a8a6acb7bf9d5f96174704
Kesav Gopan <139054897+KesavGopan10@users.noreply.github.com>
2025-07-04 02:23:20 -0700

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwindcssPostcss from '@tailwindcss/postcss';
+
+export default {
+  plugins: [
+    tailwindcssPostcss,
+    autoprefixer,
+  ],
+};
